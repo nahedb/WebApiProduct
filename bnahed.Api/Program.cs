01d822c793768e15091c106741590e17568b1922
Brendan Nahed <brendan.n.nahed@gmail.com>
2024-03-18 21:26:49 -0400

    Fixed references and reupdated Program file to work again

--- bnahed.Api/Program.cs
+++ bnahed.Api/Program.cs
@@ -1,35 +1,39 @@
-using System.Text.Json.Serialization;
+using bnahed.Api.Domain.Services.V1.Interfaces;
+using bnahed.Api.Domain;
+using bnahed.Api.Infrastructure.Repository.Services.Interfaces;
+using bnahed.Api.Infrastructure.Repository.Services;
+using bnahed.Api.Infrastructure.Repository.Settings;
 
 var builder = WebApplication.CreateSlimBuilder(args);
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDB"));
 
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();
+
+builder.Services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
+builder.Services.AddSingleton<IMongoDbService, MongoDbService>();
 
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
+app.MapHealthChecks("/HC");
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+//app.UseAuthentication();
+app.UseEndpoints(e =>
+{
+    _ = e.MapControllers();
+});
 
 app.Run();
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-
-}
